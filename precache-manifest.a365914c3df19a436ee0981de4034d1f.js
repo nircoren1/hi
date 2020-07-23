3ca98b165d706177ff3dbe4d54e6c8e659da6fe3
NirCoren <nircoren@gmail.com>
2020-07-23 21:41:13 +0300

    Updates

---
+++ precache-manifest.a365914c3df19a436ee0981de4034d1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d058403aabd4b0360892b21a4282df",
+    "url": "/hi/index.html"
+  },
+  {
+    "revision": "06c781e22b22bb4708d4",
+    "url": "/hi/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "a59e3d75cec28e991a91",
+    "url": "/hi/static/css/main.29a6cf22.chunk.css"
+  },
+  {
+    "revision": "06c781e22b22bb4708d4",
+    "url": "/hi/static/js/2.3d1d1d59.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/hi/static/js/2.3d1d1d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59e3d75cec28e991a91",
+    "url": "/hi/static/js/main.01e7382c.chunk.js"
+  },
+  {
+    "revision": "0fd027b35a220d4f5162",
+    "url": "/hi/static/js/runtime-main.3440b848.js"
+  }
+]);
